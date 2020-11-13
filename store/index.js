28f6f392753d9930a4fa4cced493cba69bab96fe
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2020-11-14 03:53:50 +0900

    majorを追加

--- store/index.js
+++ store/index.js
@@ -1,15 +1,22 @@
 export const state = () => ({
   uid: '',
+  major: '',
 });
 
 export const getters = {
   uid(state) {
     return state.uid;
   },
+  major(state) {
+    return state.major;
+  },
 };
 
 export const mutations = {
   getUid(state, payload) {
     state.uid = payload.uid;
+  },
+  getMajor(state, payload) {
+    state.major = payload.major;
   },
 };
