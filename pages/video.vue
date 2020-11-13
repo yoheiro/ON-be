cb21bc64ba6c7f3e134fecced6972392f0f951f6
takahirokaji <45552770+takahirokaji@users.noreply.github.com>
2020-11-14 02:44:48 +0900

    ミュート機能の作成 ビデオのオンオフ切り替え 退出機能の改善 入出者の表示 本人のデータ表示

--- pages/video.vue
+++ pages/video.vue
@@ -1,60 +1,121 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12">
-      <div v-for="(stream, index) in remoteStreams" :key="index">
-        <video autoplay playsinline :srcObject.prop="stream"></video>
-      </div>
-      <video
-        id="my-video"
-        muted="true"
-        width="500"
-        autoplay
-        playsinline
-      ></video>
-      <input v-model="roomId" type="text" placeholder="Room Name" />
-      <p>
-        ROOM ID: <span id="room-id">{{ roomId }}</span>
-      </p>
+  <div>
+    {{ messages }}
+    <v-row justify="center" align="center">
+      <v-col cols="6">
+        <div class="my-video-object">
+          <video id="my-video" muted="true" autoplay playsinline></video>
+          <div class="my-info">
+            <v-icon :disabled="disableMyInfo" size="20px" @click="showMyInfo"
+              >mdi-information-outline</v-icon
+            >
+          </div>
+          <v-overlay :value="myInfoOverlay" :dark="false">
+            <v-card class="my-info-card">
+              <v-btn fab small class="my-info-close" @click="closeMyInfo"
+                ><v-icon>mdi-close</v-icon></v-btn
+              >
+              <v-list>
+                <v-list-item>
+                  <v-list-item-icon class="ml-3">
+                    <v-avatar><img :src="myInfo.userIcon" /></v-avatar>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    {{ myInfo.userName }}
+                  </v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title class="ml-3" style="text-align: left"
+                      >自己紹介文</v-list-item-title
+                    >
+                    <p>{{ myInfo.profile }}</p>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-overlay>
+        </div>
+      </v-col>
+      <v-col cols="6">
+        <v-list>
+          <v-list-item>kaji</v-list-item>
+          <v-list-item>静岡大学</v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
+    <v-row>
+      <v-col v-for="(stream, index) in remoteStreams" :key="index" cols="6">
+        <div>
+          <video
+            class="partner-video"
+            autoplay
+            playsinline
+            :srcObject.prop="stream"
+          ></video>
+        </div>
+      </v-col>
       <button
-        v-if="roomOpened === true"
+        v-if="roomOpened === false"
         class="button--green"
-        @click="leaveRoom"
+        @click="joinRoom"
       >
-        Leave
+        Join
       </button>
-      <button v-else class="button--green" @click="joinRoom">Join</button>
       <br />
-      <div>
-        マイク:
-        <select v-model="selectedAudio" @change="onChange">
-          <option disabled value="">Please select one</option>
-          <option
-            v-for="(audio, key, index) in audios"
-            :key="index"
-            :value="audio.value"
-          >
-            {{ audio.text }}
-          </option>
-        </select>
+    </v-row>
+    <input v-model="roomId" type="text" placeholder="Room Name" />
+    <p>
+      ROOM ID: <span id="room-id">{{ roomId }}</span>
+    </p>
+    <div>
+      カメラ:
+      <select ref="AudioSelecter" v-model="selectedVideo" @change="onChange">
+        <option disabled value="">Please select one</option>
+        <option
+          v-for="(video, key, index) in videos"
+          :key="index"
+          :value="video.value"
+        >
+          {{ video.text }}
+        </option>
+      </select>
+    </div>
+    マイク:
+    <select v-model="selectedAudio" @change="onChange">
+      <option disabled value="">Please select one</option>
+      <option
+        v-for="(audio, key, index) in audios"
+        :key="index"
+        :value="audio.value"
+      >
+        {{ audio.text }}
+      </option>
+    </select>
+    <v-bottom-navigation fixed>
+      <v-btn @click="audioEnabled">
+        <span>オーディオ</span>
+        <v-icon>mdi-microphone</v-icon>
+      </v-btn>
 
-        カメラ:
-        <select v-model="selectedVideo" @change="onChange">
-          <option disabled value="">Please select one</option>
-          <option
-            v-for="(video, key, index) in videos"
-            :key="index"
-            :value="video.value"
-          >
-            {{ video.text }}
-          </option>
-        </select>
-      </div>
-    </v-col>
-  </v-row>
+      <v-btn @click="cameraEnabled">
+        <span>カメラ</span>
+        <v-icon>mdi-camera</v-icon>
+      </v-btn>
+
+      <v-btn @click="leaveRoom">
+        <span>退出</span>
+
+        <v-icon>mdi-door</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
 </template>
 
 <script>
 import Peer from 'skyway-js';
+import firebase from '~/plugins/firebase';
 
 export default {
   data() {
@@ -66,18 +127,28 @@ export default {
       videos: [],
       localStream: null,
       peerId: '',
-      roomId: '',
+      roomId: '1234',
       remoteStreams: [],
       roomOpened: false,
+      messages: [],
+      myInfo: {},
+      myInfoOverlay: false,
+      disableMyInfo: false,
     };
   },
   async created() {
-    // ここでpeerのリスナーを設置
-    this.peer = new Peer({ key: this.APIKey, debug: 3 }); // 新規にPeerオブジェクトの作成
+    await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true, // マイクから音声も取得する場合はtrue
+    });
+
+    this.peer = new Peer({
+      key: this.APIKey,
+      debug: 3,
+    }); // 新規にPeerオブジェクトの作成
 
     // デバイスへのアクセス
     const deviceInfos = await navigator.mediaDevices.enumerateDevices();
-    console.log(deviceInfos);
 
     // オーディオデバイスの情報を取得
     deviceInfos
@@ -104,7 +175,6 @@ export default {
     onChange() {
       if (this.selectedAudio !== '' && this.selectedVideo !== '') {
         this.connectLocalCamera();
-        console.log('onchange');
       }
     },
 
@@ -151,6 +221,8 @@ export default {
       // 重要：streamの内容に変更があった時（stream）videoタグを作って流す
       this.room.on('stream', async (stream) => {
         await this.remoteStreams.push(stream);
+        console.log('リモートストリーム');
+        console.log(this.remoteStreams);
       });
 
       // 重要：誰かがテキストメッセージを送った時、messagesを更新
@@ -160,8 +232,8 @@ export default {
 
       // 誰かが退出した場合、div（remoteVideos）内にある、任意のdata-peer-idがついたvideoタグの内容を空にして削除する
       this.room.on('peerLeave', (peerId) => {
-        // const index = this.remoteStreams.findIndex((v) => v.peerId === peerId);
-        // const removedStream = this.remoteStreams.splice(index, 1);
+        const index = this.remoteStreams.findIndex((v) => v.peerId === peerId);
+        this.remoteStreams.splice(index, 1);
         this.messages.push(`=== ${peerId} left ===`);
       });
 
@@ -171,6 +243,40 @@ export default {
         this.messages.length = 0;
       });
     },
+    async getMyData() {
+      this.myInfo = await firebase
+        .firestore()
+        .collection('users')
+        .doc('4w7Q9B9ziaPjVQnsniQftUhMoIF3')
+        .get()
+        .then((doc) => {
+          return doc.data();
+        });
+    },
+    showMyInfo() {
+      this.disableMyInfo = !this.disableMyInfo;
+      this.getMyData().then(() => {
+        this.myInfoOverlay = !this.myInfoOverlay;
+      });
+    },
+    closeMyInfo() {
+      this.myInfoOverlay = !this.myInfoOverlay;
+      this.disableMyInfo = !this.disableMyInfo;
+    },
+    cameraEnabled() {
+      const videoTrack = this.localStream.getVideoTracks()[0];
+      console.log(videoTrack);
+      videoTrack.enabled = !videoTrack.enabled;
+    },
+    audioEnabled() {
+      console.log(this.localStream.getAudioTracks());
+      const audioTrack = this.localStream.getAudioTracks()[0];
+      audioTrack.enabled = !audioTrack.enabled;
+      console.log(audioTrack.enabled);
+    },
+    test() {
+      console.log(this.$refs.AudioSelecter);
+    },
   },
 };
 </script>
@@ -178,6 +284,39 @@ export default {
 <style scoped>
 #my-video {
   transform: rotateY(180deg);
-  max-width: 40vw;
+  max-width: 100%;
+  border-radius: 10px;
+  border: solid 3px #6091d3;
+}
+
+.my-video-object,
+.my-info-card {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.my-info {
+  position: absolute;
+  text-align: right;
+  right: 3px;
+  z-index: 10;
+  width: 100%;
+}
+.my-info-close {
+  position: absolute;
+  text-align: right;
+  right: 3px;
+  z-index: 100;
+}
+.partner-video {
+  max-width: 100%;
+  border-radius: 10px;
+  border: solid 3px #6091d3;
+}
+
+.my-info-card {
+  width: 90vw;
+  border-radius: 10px;
 }
 </style>
